a5534f36a95a1b2047aaa1aa7ffc888924877403
Juan Jose Casafranca <juan.casafranca@kdab.com>
2020-02-14 19:45:41 +0100

    Dont fail if a user extension fails

--- addons/io_scene_gltf2/io/exp/gltf2_io_user_extensions.py
+++ addons/io_scene_gltf2/io/exp/gltf2_io_user_extensions.py
@@ -20,4 +20,7 @@ def export_user_extensions(hook_name, export_settings, *args):
     for extension in export_settings['gltf_user_extensions']:
         hook = getattr(extension, hook_name, None)
         if hook is not None:
-            hook(*args, export_settings)
+            try:
+                hook(*args, export_settings)
+            except Exception:
+                pass
